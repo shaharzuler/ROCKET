94c897b4001776139d22bc214d228243bb89b053
shaharzuler <shahar.zuler@gmail.com>
2021-03-20 18:24:48 +0200

    adding dataloaders saving. still lacks reference to dataloaders themselves, and take care of hyperparams

--- train.py
+++ train.py
@@ -1,5 +1,6 @@
 import os
-import pandas as pd
+import pickle
+
 import pytorch_lightning as pl
 
 from model import RocketNet
@@ -10,17 +11,38 @@ KERNEL_COUNT = 100
 TRAIN_DATA_PATH = os.path.join("data", "FordA_TRAIN.csv")
 TEST_DATA_PATH = os.path.join("data", "FordA_TEST.csv")
 TRAINED_MODEL_PATH = os.path.join("trained_models")
+DATALOADERS_PATH = os.path.join("dataloaders")
+
+
+def make_dataloaders():
+    #TEMP. replace with dataloaders
+    pass
+
+
+def save_dataloaders():
+    os.makedirs(DATALOADERS_PATH, exist_ok=True)
+    train_dl_path = os.path.join(DATALOADERS_PATH, "train_dl.pkl")
+    test_dl_path = os.path.join(DATALOADERS_PATH, "test_dl.pkl")
+    with open(train_dl_path, "wb") as fp:
+        pickle.dump(train_dataloader, fp)
+    with open(test_dl_path, "wb") as fp:
+        pickle.dump(val_dataloader, fp)
 
 
 if __name__ == '__main__':
     df_train = load_data(TRAIN_DATA_PATH)
     df_test = load_data(TEST_DATA_PATH)
 
-    model = RocketNet(
-        n_classes=N_CLASSES,
-        kernel_count=KERNEL_COUNT,
-        kernel_config=None
-    )
+    train_dataloader, val_dataloader = make_dataloaders()
+
+    save_dataloaders()
+
+    model = RocketNet(x_dim=20,
+                      n_classes=N_CLASSES,
+                      kernel_count=KERNEL_COUNT,
+                      max_sequence_len=100,
+                      kernel_lengths=[7, 9, 11])
+
 
     checkpoint_cb = pl.callbacks.ModelCheckpoint(
         dirpath=TRAINED_MODEL_PATH,
